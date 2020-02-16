43574e448e320bbdf030321863fd5ad3f0249663
Davide Albiero <davide2910@live.it>
2020-02-16 18:24:23 +0100

    Removed this

--- src/main/java/edivad/dimstorage/client/model/ModelDimChest.java
+++ src/main/java/edivad/dimstorage/client/model/ModelDimChest.java
@@ -22,59 +22,59 @@ public class ModelDimChest extends Model {
 		this.textureWidth = 128;
 		this.textureHeight = 128;
 
-		this.base = new RendererModel(this, 0, 0);
-		this.base.addBox(0F, 0F, 0F, 16, 13, 16);
-		this.base.setRotationPoint(-8F, 11F, -8F);
-		this.base.setTextureSize(128, 128);
-		this.base.mirror = true;
-		this.setRotation(this.base, 0F, 0F, 0F);
+		base = new RendererModel(this, 0, 0);
+		base.addBox(0F, 0F, 0F, 16, 13, 16);
+		base.setRotationPoint(-8F, 11F, -8F);
+		base.setTextureSize(128, 128);
+		base.mirror = true;
+		setRotation(this.base, 0F, 0F, 0F);
 
-		this.top1 = new RendererModel(this, 66, 2);
-		this.top1.addBox(0F, 0F, 0F, 16, 3, 2);
-		this.top1.setRotationPoint(-8F, 8F, -8F);
-		this.top1.setTextureSize(128, 128);
-		this.top1.mirror = true;
-		this.setRotation(this.top1, 0F, 0F, 0F);
+		top1 = new RendererModel(this, 66, 2);
+		top1.addBox(0F, 0F, 0F, 16, 3, 2);
+		top1.setRotationPoint(-8F, 8F, -8F);
+		top1.setTextureSize(128, 128);
+		top1.mirror = true;
+		setRotation(this.top1, 0F, 0F, 0F);
 
-		this.top2 = new RendererModel(this, 0, 32);
-		this.top2.addBox(0F, 0F, 0F, 2, 3, 14);
-		this.top2.setRotationPoint(6F, 8F, -6F);
-		this.top2.setTextureSize(128, 128);
-		this.top2.mirror = true;
-		this.setRotation(this.top2, 0F, 0F, 0F);
+		top2 = new RendererModel(this, 0, 32);
+		top2.addBox(0F, 0F, 0F, 2, 3, 14);
+		top2.setRotationPoint(6F, 8F, -6F);
+		top2.setTextureSize(128, 128);
+		top2.mirror = true;
+		setRotation(this.top2, 0F, 0F, 0F);
 
-		this.top3 = new RendererModel(this, 36, 32);
-		this.top3.addBox(0F, 0F, 0F, 2, 3, 14);
-		this.top3.setRotationPoint(-8F, 8F, -6F);
-		this.top3.setTextureSize(128, 128);
-		this.top3.mirror = true;
-		this.setRotation(this.top3, 0F, 0F, 0F);
+		top3 = new RendererModel(this, 36, 32);
+		top3.addBox(0F, 0F, 0F, 2, 3, 14);
+		top3.setRotationPoint(-8F, 8F, -6F);
+		top3.setTextureSize(128, 128);
+		top3.mirror = true;
+		setRotation(this.top3, 0F, 0F, 0F);
 
-		this.top4 = new RendererModel(this, 66, 10);
-		this.top4.addBox(0F, 0F, 0F, 12, 3, 2);
-		this.top4.setRotationPoint(-6F, 8F, 6F);
-		this.top4.setTextureSize(128, 128);
-		this.top4.mirror = true;
-		this.setRotation(this.top4, 0F, 0F, 0F);
+		top4 = new RendererModel(this, 66, 10);
+		top4.addBox(0F, 0F, 0F, 12, 3, 2);
+		top4.setRotationPoint(-6F, 8F, 6F);
+		top4.setTextureSize(128, 128);
+		top4.mirror = true;
+		setRotation(this.top4, 0F, 0F, 0F);
 
-		this.top5 = new RendererModel(this, 72, 32);
-		this.top5.addBox(0F, 0F, 0F, 12, 2, 6);
-		this.top5.setRotationPoint(-6F, 8F, 0F);
-		this.top5.setTextureSize(128, 128);
-		this.top5.mirror = true;
-		this.setRotation(this.top5, 0F, 0F, 0F);
+		top5 = new RendererModel(this, 72, 32);
+		top5.addBox(0F, 0F, 0F, 12, 2, 6);
+		top5.setRotationPoint(-6F, 8F, 0F);
+		top5.setTextureSize(128, 128);
+		top5.mirror = true;
+		setRotation(this.top5, 0F, 0F, 0F);
 
-		this.movable = new RendererModel(this, 70, 24);
-		this.movable.addBox(0F, 0F, 0F, 12, 1, 6);
-		this.movable.setRotationPoint(-6F, 8.533334F, -6F);
-		this.movable.setTextureSize(128, 128);
-		this.movable.mirror = true;
-		this.setRotation(this.movable, 0F, 0F, 0F);
+		movable = new RendererModel(this, 70, 24);
+		movable.addBox(0F, 0F, 0F, 12, 1, 6);
+		movable.setRotationPoint(-6F, 8.533334F, -6F);
+		movable.setTextureSize(128, 128);
+		movable.mirror = true;
+		setRotation(this.movable, 0F, 0F, 0F);
 
-		this.indicatorGreen = this.createIndicator(0);
-		this.indicatorBlue = this.createIndicator(2);
-		this.indicatorRed = this.createIndicator(4);
-		this.indicatorYellow = this.createIndicator(6);
+		indicatorGreen = this.createIndicator(0);
+		indicatorBlue = this.createIndicator(2);
+		indicatorRed = this.createIndicator(4);
+		indicatorYellow = this.createIndicator(6);
 	}
 
 	public void render(float f)
@@ -88,18 +88,18 @@ public class ModelDimChest extends Model {
 		top5.render(f);
 
 		// render movable part
-		this.movable.offsetZ = this.tileEntity.movablePartState;
-		this.movable.render(f);
+		movable.offsetZ = tileEntity.movablePartState;
+		movable.render(f);
 
 		// check state
-		if(this.tileEntity.locked)
-			this.indicatorRed.render(f);
-		else if(this.tileEntity.frequency.hasOwner())
-			this.indicatorBlue.render(f);
-		else if(this.tileEntity.collect)
-			this.indicatorYellow.render(f);
+		if(tileEntity.locked)
+			indicatorRed.render(f);
+		else if(tileEntity.frequency.hasOwner())
+			indicatorBlue.render(f);
+		else if(tileEntity.collect)
+			indicatorYellow.render(f);
 		else
-			this.indicatorGreen.render(f);
+			indicatorGreen.render(f);
 	}
 
 	private RendererModel createIndicator(int offsetY)
