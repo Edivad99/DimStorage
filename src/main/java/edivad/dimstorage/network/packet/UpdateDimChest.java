703bdd30f36c3a340fe98c8e43478587908f8c9c
Davide Albiero <davide2910@live.it>
2020-03-31 14:29:13 +0200

    Update network handler

--- src/main/java/edivad/dimstorage/network/packet/UpdateDimChest.java
+++ src/main/java/edivad/dimstorage/network/packet/UpdateDimChest.java
@@ -1,77 +1,56 @@
 package edivad.dimstorage.network.packet;
 
-import java.util.function.Supplier;
-
 import edivad.dimstorage.Main;
-import edivad.dimstorage.api.Frequency;
 import edivad.dimstorage.tile.TileEntityDimChest;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants.BlockFlags;
-import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-public class UpdateDimChest {
+public class UpdateDimChest extends UpdateDimBase {
 
-	private BlockPos pos;
-	private Frequency freq;
-	private boolean locked, collect;
+	protected boolean collect;
 
 	public UpdateDimChest(PacketBuffer buf)
 	{
-		pos = buf.readBlockPos();
-		freq = Frequency.readFromPacket(buf);
-		locked = buf.readBoolean();
+		super(buf);
 		collect = buf.readBoolean();
 	}
 
 	public UpdateDimChest(TileEntityDimChest tile)
 	{
-		pos = tile.getPos();
-		freq = tile.frequency;
-		locked = tile.locked;
+		super(tile);
 		collect = tile.collect;
 	}
 
+	@Override
 	public void toBytes(PacketBuffer buf)
 	{
-		buf.writeBlockPos(pos);
-
-		freq.writeToPacket(buf);
-
-		buf.writeBoolean(locked);
+		super.toBytes(buf);
 		buf.writeBoolean(collect);
 	}
 
-	public void handle(Supplier<NetworkEvent.Context> ctx)
+	@Override
+	public void customHandle(World world, PlayerEntity player)
 	{
-		ctx.get().enqueueWork(() -> {
-			PlayerEntity player = ctx.get().getSender();
-			World world = player.world;
-			if(!world.isBlockPresent(pos))
-				return;
+		TileEntity tile = world.getTileEntity(pos);
 
-			TileEntity tile = world.getTileEntity(pos);
+		if(!(tile instanceof TileEntityDimChest))
+		{
+			Main.logger.error("Wrong type of tile entity (expected TileEntityDimChest)!");
+			return;
+		}
+		
+		TileEntityDimChest chest = (TileEntityDimChest) tile;
+		chest.setFreq(freq);
+		chest.locked = locked;
+		chest.collect = collect;
 
-			if(!(tile instanceof TileEntityDimChest))
-			{
-				Main.logger.error("Wrong type of tile entity (expected TileEntityDimChest)!");
-				return;
-			}
-			
-			TileEntityDimChest chest = (TileEntityDimChest) tile;
-			chest.frequency.set(freq);
-			chest.locked = locked;
-			chest.collect = collect;
-
-			world.notifyBlockUpdate(pos, chest.getBlockState(), chest.getBlockState(), BlockFlags.DEFAULT);
-			NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) chest, buf -> buf.writeBlockPos(pos).writeBoolean(true));
-		});
-		ctx.get().setPacketHandled(true);
+		world.notifyBlockUpdate(pos, chest.getBlockState(), chest.getBlockState(), BlockFlags.DEFAULT);
+		NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) chest, buf -> buf.writeBlockPos(pos).writeBoolean(true));
 	}
 }
--- src/main/java/edivad/dimstorage/network/packet/UpdateDimTank.java
+++ src/main/java/edivad/dimstorage/network/packet/UpdateDimTank.java
@@ -1,72 +1,44 @@
 package edivad.dimstorage.network.packet;
 
-import java.util.function.Supplier;
-
 import edivad.dimstorage.Main;
-import edivad.dimstorage.api.Frequency;
 import edivad.dimstorage.tile.TileEntityDimTank;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants.BlockFlags;
-import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-public class UpdateDimTank {
-
-	private BlockPos pos;
-	private Frequency freq;
-	private boolean locked;
+public class UpdateDimTank extends UpdateDimBase {
 
 	public UpdateDimTank(PacketBuffer buf)
 	{
-		pos = buf.readBlockPos();
-		freq = Frequency.readFromPacket(buf);
-		locked = buf.readBoolean();
+		super(buf);
 	}
 
 	public UpdateDimTank(TileEntityDimTank tile)
 	{
-		pos = tile.getPos();
-		freq = tile.frequency;
-		locked = tile.locked;
+		super(tile);
 	}
 
-	public void toBytes(PacketBuffer buf)
+	@Override
+	public void customHandle(World world, PlayerEntity player)
 	{
-		buf.writeBlockPos(pos);
-		freq.writeToPacket(buf);
-		buf.writeBoolean(locked);
-	}
+		TileEntity tile = world.getTileEntity(pos);
 
-	public void handle(Supplier<NetworkEvent.Context> ctx)
-	{
-		ctx.get().enqueueWork(() ->
+		if(!(tile instanceof TileEntityDimTank))
 		{
-			PlayerEntity player = ctx.get().getSender();
-			World world = player.world;
-			if(!world.isBlockPresent(pos))
-				return;
+			Main.logger.error("Wrong type of tile entity (expected TileEntityDimTank)!");
+			return;
+		}
+		
+		TileEntityDimTank tank = (TileEntityDimTank) tile;
+		tank.setFreq(freq);
+		tank.locked = locked;
 
-			TileEntity tile = world.getTileEntity(pos);
-
-			if(!(tile instanceof TileEntityDimTank))
-			{
-				Main.logger.error("Wrong type of tile entity (expected TileEntityDimTank)!");
-				return;
-			}
-			
-			TileEntityDimTank tank = (TileEntityDimTank) tile;
-			tank.setFreq(freq);
-			tank.locked = locked;
-
-			world.notifyBlockUpdate(pos, tank.getBlockState(), tank.getBlockState(), BlockFlags.DEFAULT);
-			NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) tank, buf -> buf.writeBlockPos(pos).writeBoolean(true));
-		});
-		ctx.get().setPacketHandled(true);
+		world.notifyBlockUpdate(pos, tank.getBlockState(), tank.getBlockState(), BlockFlags.DEFAULT);
+		NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) tank, buf -> buf.writeBlockPos(pos).writeBoolean(true));
 	}
 }
