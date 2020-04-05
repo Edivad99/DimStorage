d6f8d14e666dfb9d393bead7f7bb055c01501802
Davide Albiero <davide2910@live.it>
2020-04-05 16:58:33 +0200

    The direction of the packets is specified
    Updated Forge min version

--- src/main/java/edivad/dimstorage/network/PacketHandler.java
+++ src/main/java/edivad/dimstorage/network/PacketHandler.java
@@ -1,11 +1,14 @@
 package edivad.dimstorage.network;
 
+import java.util.Optional;
+
 import edivad.dimstorage.Main;
 import edivad.dimstorage.network.packet.OpenChest;
 import edivad.dimstorage.network.packet.SyncLiquidTank;
 import edivad.dimstorage.network.packet.UpdateDimChest;
 import edivad.dimstorage.network.packet.UpdateDimTank;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
@@ -22,10 +25,10 @@ public class PacketHandler {
 	public static void init()
 	{
 		int id = 0;
-		INSTANCE.registerMessage(id++, UpdateDimChest.class, UpdateDimChest::toBytes, UpdateDimChest::new, UpdateDimChest::handle);
-		INSTANCE.registerMessage(id++, OpenChest.class, OpenChest::toBytes, OpenChest::new, OpenChest::handle);
+		INSTANCE.registerMessage(id++, UpdateDimChest.class, UpdateDimChest::toBytes, UpdateDimChest::new, UpdateDimChest::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
+		INSTANCE.registerMessage(id++, OpenChest.class, OpenChest::toBytes, OpenChest::new, OpenChest::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
 
-		INSTANCE.registerMessage(id++, UpdateDimTank.class, UpdateDimTank::toBytes, UpdateDimTank::new, UpdateDimTank::handle);
-		INSTANCE.registerMessage(id++, SyncLiquidTank.class, SyncLiquidTank::toBytes, SyncLiquidTank::new, SyncLiquidTank::handle);
+		INSTANCE.registerMessage(id++, UpdateDimTank.class, UpdateDimTank::toBytes, UpdateDimTank::new, UpdateDimTank::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
+		INSTANCE.registerMessage(id++, SyncLiquidTank.class, SyncLiquidTank::toBytes, SyncLiquidTank::new, SyncLiquidTank::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
 	};
 }
