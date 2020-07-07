93066cc0681bd2229575be75edc15eb6c573fe00
Davide Albiero <davide2910@live.it>
2020-07-07 10:50:06 +0200

    Add loot tables

--- src/main/java/edivad/dimstorage/items/ItemDimBase.java
+++ src/main/java/edivad/dimstorage/items/ItemDimBase.java
@@ -33,8 +33,8 @@ public class ItemDimBase extends BlockItem {
     {
         if(stack.hasTag())
         {
-            CompoundNBT stackTag = stack.getTag();
-            if(stackTag.contains("Frequency"))
+            CompoundNBT stackTag = stack.getChildTag("DimStorage");
+            if(stackTag != null && stackTag.contains("Frequency"))
             {
                 return new Frequency(stackTag.getCompound("Frequency"));
             }
