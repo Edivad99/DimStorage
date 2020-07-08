b78912d30ea4e7a775c9f4b87062fd5a7d4a0a69
Davide Albiero <davide2910@live.it>
2020-07-08 10:06:17 +0200

    Enable theoneprobe for 1.16.1

--- src/main/java/edivad/dimstorage/compat/MainCompatHandler.java
+++ src/main/java/edivad/dimstorage/compat/MainCompatHandler.java
@@ -1,6 +1,6 @@
 package edivad.dimstorage.compat;
 
-//import edivad.dimstorage.compat.top.TOPProvider;
+import edivad.dimstorage.compat.top.TOPProvider;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 
@@ -8,9 +8,9 @@ public class MainCompatHandler {
 
     public static void registerTOP()
     {
-//        if(ModList.get().isLoaded("theoneprobe"))
-//        {
-//            InterModComms.sendTo("theoneprobe", "getTheOneProbe", TOPProvider::new);
-//        }
+        if(ModList.get().isLoaded("theoneprobe"))
+        {
+            InterModComms.sendTo("theoneprobe", "getTheOneProbe", TOPProvider::new);
+        }
     }
 }
