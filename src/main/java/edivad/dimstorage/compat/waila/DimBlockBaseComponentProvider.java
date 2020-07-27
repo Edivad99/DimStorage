e6362dbb5545cdea4a731de2c32a5ece046bf669
Davide Albiero <davide2910@live.it>
2020-07-27 09:21:47 +0200

    Added Waila compat

--- src/main/java/edivad/dimstorage/compat/waila/DimBlockBaseComponentProvider.java
+++ src/main/java/edivad/dimstorage/compat/waila/DimBlockBaseComponentProvider.java
@@ -1,42 +1,42 @@
 package edivad.dimstorage.compat.waila;
 
-//import java.util.List;
-//
-//import edivad.dimstorage.Main;
-//import edivad.dimstorage.tile.TileFrequencyOwner;
-//import mcp.mobius.waila.api.IComponentProvider;
-//import mcp.mobius.waila.api.IDataAccessor;
-//import mcp.mobius.waila.api.IPluginConfig;
-//import net.minecraft.nbt.CompoundNBT;
-//import net.minecraft.util.text.ITextComponent;
-//import net.minecraft.util.text.StringTextComponent;
-//import net.minecraft.util.text.TextFormatting;
-//import net.minecraft.util.text.TranslationTextComponent;
-//
-//public class DimBlockBaseComponentProvider implements IComponentProvider {
-//
-//    @Override
-//    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config)
-//    {
-//        if(accessor.getTileEntity() instanceof TileFrequencyOwner)
-//        {
-//            CompoundNBT data = accessor.getServerData();
-//
-//            String owner = new TranslationTextComponent("gui." + Main.MODID + ".owner").getString() + " ";
-//            String freq = new TranslationTextComponent("gui." + Main.MODID + ".frequency").getString() + " ";
-//            String locked = new TranslationTextComponent("gui." + Main.MODID + ".locked").getString() + " ";
-//            String yes = new TranslationTextComponent("gui." + Main.MODID + ".yes").getString();
-//
-//            if(data.getBoolean("HasOwner"))
-//            {
-//                if(data.getBoolean("CanAccess"))
-//                    tooltip.add(new StringTextComponent(TextFormatting.GREEN + owner + data.getString("Owner")));
-//                else
-//                    tooltip.add(new StringTextComponent(TextFormatting.RED + owner + data.getString("Owner")));
-//            }
-//            tooltip.add(new StringTextComponent(freq + data.getInt("Frequency")));
-//            if(data.getBoolean("Locked"))
-//                tooltip.add(new StringTextComponent(locked + yes));
-//        }
-//    }
-//}
+import java.util.List;
+
+import edivad.dimstorage.Main;
+import edivad.dimstorage.tile.TileFrequencyOwner;
+import mcp.mobius.waila.api.IComponentProvider;
+import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IPluginConfig;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public class DimBlockBaseComponentProvider implements IComponentProvider {
+
+    @Override
+    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config)
+    {
+        if(accessor.getTileEntity() instanceof TileFrequencyOwner)
+        {
+            CompoundNBT data = accessor.getServerData();
+
+            String owner = new TranslationTextComponent("gui." + Main.MODID + ".owner").getString() + " ";
+            String freq = new TranslationTextComponent("gui." + Main.MODID + ".frequency").getString() + " ";
+            String locked = new TranslationTextComponent("gui." + Main.MODID + ".locked").getString() + " ";
+            String yes = new TranslationTextComponent("gui." + Main.MODID + ".yes").getString();
+
+            if(data.getBoolean("HasOwner"))
+            {
+                if(data.getBoolean("CanAccess"))
+                    tooltip.add(new StringTextComponent(TextFormatting.GREEN + owner + data.getString("Owner")));
+                else
+                    tooltip.add(new StringTextComponent(TextFormatting.RED + owner + data.getString("Owner")));
+            }
+            tooltip.add(new StringTextComponent(freq + data.getInt("Frequency")));
+            if(data.getBoolean("Locked"))
+                tooltip.add(new StringTextComponent(locked + yes));
+        }
+    }
+}
--- src/main/java/edivad/dimstorage/compat/waila/DimTankComponentProvider.java
+++ src/main/java/edivad/dimstorage/compat/waila/DimTankComponentProvider.java
@@ -1,40 +1,40 @@
 package edivad.dimstorage.compat.waila;
 
-//import java.util.List;
-//
-//import edivad.dimstorage.Main;
-//import edivad.dimstorage.tile.TileEntityDimTank;
-//import mcp.mobius.waila.api.IComponentProvider;
-//import mcp.mobius.waila.api.IDataAccessor;
-//import mcp.mobius.waila.api.IPluginConfig;
-//import net.minecraft.nbt.CompoundNBT;
-//import net.minecraft.util.text.ITextComponent;
-//import net.minecraft.util.text.StringTextComponent;
-//import net.minecraft.util.text.TranslationTextComponent;
-//
-//public class DimTankComponentProvider extends DimBlockBaseComponentProvider implements IComponentProvider {
-//
-//    @Override
-//    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config)
-//    {
-//        super.appendBody(tooltip, accessor, config);
-//        if(accessor.getTileEntity() instanceof TileEntityDimTank)
-//        {
-//            CompoundNBT data = accessor.getServerData();
-//
-//            String autoEject = new TranslationTextComponent("gui." + Main.MODID + ".eject").getString() + ": ";
-//            String yes = new TranslationTextComponent("gui." + Main.MODID + ".yes").getString();
-//            String liquid = new TranslationTextComponent("gui." + Main.MODID + ".liquid").getString();
-//            String amount = new TranslationTextComponent("gui." + Main.MODID + ".amount").getString();
-//
-//            if(data.getBoolean("AutoEject"))
-//                tooltip.add(new StringTextComponent(autoEject + yes));
-//            if(data.getInt("Amount") > 0)
-//            {
-//                tooltip.add(new StringTextComponent(liquid + " " + data.getString("Liquid")));
-//                tooltip.add(new StringTextComponent(amount + " " + data.getInt("Amount") + " mB"));
-//            }
-//
-//        }
-//    }
-//}
+import java.util.List;
+
+import edivad.dimstorage.Main;
+import edivad.dimstorage.tile.TileEntityDimTank;
+import mcp.mobius.waila.api.IComponentProvider;
+import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IPluginConfig;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public class DimTankComponentProvider extends DimBlockBaseComponentProvider implements IComponentProvider {
+
+    @Override
+    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config)
+    {
+        super.appendBody(tooltip, accessor, config);
+        if(accessor.getTileEntity() instanceof TileEntityDimTank)
+        {
+            CompoundNBT data = accessor.getServerData();
+
+            String autoEject = new TranslationTextComponent("gui." + Main.MODID + ".eject").getString() + ": ";
+            String yes = new TranslationTextComponent("gui." + Main.MODID + ".yes").getString();
+            String liquid = new TranslationTextComponent("gui." + Main.MODID + ".liquid").getString();
+            String amount = new TranslationTextComponent("gui." + Main.MODID + ".amount").getString();
+
+            if(data.getBoolean("AutoEject"))
+                tooltip.add(new StringTextComponent(autoEject + yes));
+            if(data.getInt("Amount") > 0)
+            {
+                tooltip.add(new StringTextComponent(liquid + " " + data.getString("Liquid")));
+                tooltip.add(new StringTextComponent(amount + " " + data.getInt("Amount") + " mB"));
+            }
+
+        }
+    }
+}
