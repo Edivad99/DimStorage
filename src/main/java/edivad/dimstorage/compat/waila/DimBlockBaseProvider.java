4d2c2113c572c5b824d5b2f65d3ce8cb331c6c56
Davide Albiero <davide2910@live.it>
2021-12-02 11:24:56 +0100

    Waila compat

--- src/main/java/edivad/dimstorage/compat/waila/DimBlockBaseProvider.java
+++ src/main/java/edivad/dimstorage/compat/waila/DimBlockBaseProvider.java
@@ -1,25 +1,25 @@
-//package edivad.dimstorage.compat.waila;
-//
-//import edivad.dimstorage.Main;
-//import edivad.dimstorage.api.Frequency;
-//import edivad.dimstorage.blockentities.BlockEntityFrequencyOwner;
-//import mcp.mobius.waila.api.IServerDataProvider;
-//import net.minecraft.nbt.CompoundTag;
-//import net.minecraft.server.level.ServerPlayer;
-//import net.minecraft.world.level.Level;
-//import net.minecraft.world.level.block.entity.BlockEntity;
-//
-//public class DimBlockBaseProvider implements IServerDataProvider<BlockEntity> {
-//
-//    @Override
-//    public void appendServerData(CompoundTag compoundTag, ServerPlayer serverPlayer, Level level, BlockEntity tileEntity, boolean showDetails) {
-//        if(tileEntity instanceof BlockEntityFrequencyOwner tile) {
-//            Frequency frequency = tile.getFrequency();
-//            compoundTag.putBoolean(Main.MODID + ".HasOwner", frequency.hasOwner());
-//            compoundTag.putBoolean(Main.MODID + ".CanAccess", tile.canAccess(serverPlayer));
-//            compoundTag.putString(Main.MODID + ".Owner", frequency.getOwner());
-//            compoundTag.putInt(Main.MODID + ".Frequency", frequency.getChannel());
-//            compoundTag.putBoolean(Main.MODID + ".Locked", tile.locked);
-//        }
-//    }
-//}
+package edivad.dimstorage.compat.waila;
+
+import edivad.dimstorage.Main;
+import edivad.dimstorage.api.Frequency;
+import edivad.dimstorage.blockentities.BlockEntityFrequencyOwner;
+import mcp.mobius.waila.api.IServerDataProvider;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class DimBlockBaseProvider implements IServerDataProvider<BlockEntity> {
+
+    @Override
+    public void appendServerData(CompoundTag compoundTag, ServerPlayer serverPlayer, Level level, BlockEntity tileEntity, boolean showDetails) {
+        if(tileEntity instanceof BlockEntityFrequencyOwner tile) {
+            Frequency frequency = tile.getFrequency();
+            compoundTag.putBoolean(Main.MODID + ".HasOwner", frequency.hasOwner());
+            compoundTag.putBoolean(Main.MODID + ".CanAccess", tile.canAccess(serverPlayer));
+            compoundTag.putString(Main.MODID + ".Owner", frequency.getOwner());
+            compoundTag.putInt(Main.MODID + ".Frequency", frequency.getChannel());
+            compoundTag.putBoolean(Main.MODID + ".Locked", tile.locked);
+        }
+    }
+}
--- src/main/java/edivad/dimstorage/compat/waila/DimTankDataProvider.java
+++ src/main/java/edivad/dimstorage/compat/waila/DimTankDataProvider.java
@@ -1,24 +1,24 @@
-//package edivad.dimstorage.compat.waila;
-//
-//import edivad.dimstorage.Main;
-//import edivad.dimstorage.blockentities.BlockEntityDimTank;
-//import mcp.mobius.waila.api.IServerDataProvider;
-//import net.minecraft.nbt.CompoundTag;
-//import net.minecraft.server.level.ServerPlayer;
-//import net.minecraft.world.level.Level;
-//import net.minecraft.world.level.block.entity.BlockEntity;
-//
-//public class DimTankDataProvider extends DimBlockBaseProvider implements IServerDataProvider<BlockEntity> {
-//
-//    @Override
-//    public void appendServerData(CompoundTag compoundTag, ServerPlayer serverPlayer, Level level, BlockEntity tileEntity, boolean showDetails) {
-//        super.appendServerData(compoundTag, serverPlayer, level, tileEntity, showDetails);
-//        if(tileEntity instanceof BlockEntityDimTank tile) {
-//            compoundTag.putBoolean(Main.MODID + ".AutoEject", tile.autoEject);
-//            String liquidName = tile.liquidState.serverLiquid.getDisplayName().getString();
-//            int liquidLevel = tile.liquidState.serverLiquid.getAmount();
-//            compoundTag.putString(Main.MODID + ".Liquid", liquidName);
-//            compoundTag.putInt(Main.MODID + ".Amount", liquidLevel);
-//        }
-//    }
-//}
+package edivad.dimstorage.compat.waila;
+
+import edivad.dimstorage.Main;
+import edivad.dimstorage.blockentities.BlockEntityDimTank;
+import mcp.mobius.waila.api.IServerDataProvider;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class DimTankDataProvider extends DimBlockBaseProvider implements IServerDataProvider<BlockEntity> {
+
+    @Override
+    public void appendServerData(CompoundTag compoundTag, ServerPlayer serverPlayer, Level level, BlockEntity tileEntity, boolean showDetails) {
+        super.appendServerData(compoundTag, serverPlayer, level, tileEntity, showDetails);
+        if(tileEntity instanceof BlockEntityDimTank tile) {
+            compoundTag.putBoolean(Main.MODID + ".AutoEject", tile.autoEject);
+            String liquidName = tile.liquidState.serverLiquid.getDisplayName().getString();
+            int liquidLevel = tile.liquidState.serverLiquid.getAmount();
+            compoundTag.putString(Main.MODID + ".Liquid", liquidName);
+            compoundTag.putInt(Main.MODID + ".Amount", liquidLevel);
+        }
+    }
+}
