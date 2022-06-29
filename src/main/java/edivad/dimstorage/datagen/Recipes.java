fbc0d6a6a2d01e4cfee799e87aca82b6efbde4ed
Davide Albiero <davide2910@live.it>
2022-06-29 21:39:51 +0200

    Run datagen

--- src/main/java/edivad/dimstorage/datagen/Recipes.java
+++ src/main/java/edivad/dimstorage/datagen/Recipes.java
@@ -19,70 +19,64 @@ public class Recipes extends RecipeProvider {
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
 
-        ShapedRecipeBuilder.shaped(Registration.DIMCORE.get())//
-                .pattern("aba")//
-                .pattern("bcb")//
-                .pattern("aba")//
-                .define('a', Items.IRON_INGOT)//
-                .define('b', Items.REDSTONE)//
-                .define('c', Items.DIAMOND)//
-                //.setGroup(Main.MODID)//
-                .unlockedBy("diamond", has(Items.DIAMOND))//
+        ShapedRecipeBuilder.shaped(Registration.DIMCORE.get())
+                .pattern("aba")
+                .pattern("bcb")
+                .pattern("aba")
+                .define('a', Items.IRON_INGOT)
+                .define('b', Items.REDSTONE)
+                .define('c', Items.DIAMOND)
+                .unlockedBy(getHasName(Items.DIAMOND), has(Items.DIAMOND))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(Registration.DIMWALL.get(), 4)//
-                .pattern("aba")//
-                .pattern("bcb")//
-                .pattern("aba")//
-                .define('a', Items.IRON_INGOT)//
-                .define('b', Items.REDSTONE)//
-                .define('c', Items.ENDER_PEARL)//
-                //.setGroup(Main.MODID)//
-                .unlockedBy("ender_pearl", has(Items.ENDER_PEARL))//
+        ShapedRecipeBuilder.shaped(Registration.DIMWALL.get(), 4)
+                .pattern("aba")
+                .pattern("bcb")
+                .pattern("aba")
+                .define('a', Items.IRON_INGOT)
+                .define('b', Items.REDSTONE)
+                .define('c', Items.ENDER_PEARL)
+                .unlockedBy(getHasName(Items.ENDER_PEARL), has(Items.ENDER_PEARL))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(Registration.SOLIDDIMCORE.get())//
-                .pattern("aaa")//
-                .pattern("aba")//
-                .pattern("aaa")//
-                .define('a', Items.IRON_INGOT)//
-                .define('b', Registration.DIMCORE.get())//
-                //.setGroup(Main.MODID)//
-                .unlockedBy(Registration.DIMCORE.getId().getPath(), has(Registration.DIMCORE.get()))//
+        ShapedRecipeBuilder.shaped(Registration.SOLIDDIMCORE.get())
+                .pattern("aaa")
+                .pattern("aba")
+                .pattern("aaa")
+                .define('a', Items.IRON_INGOT)
+                .define('b', Registration.DIMCORE.get())
+                .unlockedBy(getHasName(Registration.DIMCORE.get()), has(Registration.DIMCORE.get()))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(Registration.DIMCHEST.get())//
-                .pattern("aaa")//
-                .pattern("aba")//
-                .pattern("aaa")//
-                .define('a', Registration.DIMWALL.get())//
-                .define('b', Registration.SOLIDDIMCORE.get())//
-                //.setGroup(Main.MODID)//
-                .unlockedBy(Registration.SOLIDDIMCORE.getId().getPath(), has(Registration.SOLIDDIMCORE.get()))//
+        ShapedRecipeBuilder.shaped(Registration.DIMCHEST.get())
+                .pattern("aaa")
+                .pattern("aba")
+                .pattern("aaa")
+                .define('a', Registration.DIMWALL.get())
+                .define('b', Registration.SOLIDDIMCORE.get())
+                .unlockedBy(getHasName(Registration.SOLIDDIMCORE.get()), has(Registration.SOLIDDIMCORE.get()))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(Registration.DIMTABLET.get())//
-                .pattern("cdc")//
-                .pattern("cdc")//
-                .pattern("aba")//
-                .define('a', Items.OBSIDIAN)//
-                .define('b', Registration.SOLIDDIMCORE.get())//
-                .define('c', Items.IRON_INGOT)//
-                .define('d', Tags.Items.GLASS_PANES)//
-                //.setGroup(Main.MODID)//
-                .unlockedBy(Registration.DIMCHEST.getId().getPath(), has(Registration.DIMCHEST.get()))//
+        ShapedRecipeBuilder.shaped(Registration.DIMTABLET.get())
+                .pattern("cdc")
+                .pattern("cdc")
+                .pattern("aba")
+                .define('a', Items.OBSIDIAN)
+                .define('b', Registration.SOLIDDIMCORE.get())
+                .define('c', Items.IRON_INGOT)
+                .define('d', Tags.Items.GLASS_PANES)
+                .unlockedBy(getHasName(Registration.DIMCHEST.get()), has(Registration.DIMCHEST.get()))
                 .save(consumer);
 
-        ShapedRecipeBuilder.shaped(Registration.DIMTANK.get())//
-                .pattern("ada")//
-                .pattern("dcd")//
-                .pattern("aba")//
-                .define('a', Registration.DIMWALL.get())//
-                .define('b', Registration.SOLIDDIMCORE.get())//
-                .define('c', Items.CAULDRON)//
-                .define('d', Tags.Items.GLASS)//
-                //.setGroup(Main.MODID)//
-                .unlockedBy("cauldron", has(Items.CAULDRON))//
+        ShapedRecipeBuilder.shaped(Registration.DIMTANK.get())
+                .pattern("ada")
+                .pattern("dcd")
+                .pattern("aba")
+                .define('a', Registration.DIMWALL.get())
+                .define('b', Registration.SOLIDDIMCORE.get())
+                .define('c', Items.CAULDRON)
+                .define('d', Tags.Items.GLASS)
+                .unlockedBy(getHasName(Items.CAULDRON), has(Items.CAULDRON))
                 .save(consumer);
     }
 }
