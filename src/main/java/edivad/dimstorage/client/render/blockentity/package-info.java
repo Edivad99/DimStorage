d6430024aa92f382b6ecd6c81a2a3dc0f08a148d
Davide Albiero <davide2910@live.it>
2025-12-14 16:29:54 +0100

    Use mod_dev_gradle

---
+++ src/main/java/edivad/dimstorage/client/render/blockentity/package-info.java
@@ -0,0 +1,8 @@
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package edivad.dimstorage.client.render.blockentity;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
