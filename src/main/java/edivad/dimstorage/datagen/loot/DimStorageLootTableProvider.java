7226c8dfabcd39b7e21920e0ebf7af7c697d5f53
Davide Albiero <davide2910@live.it>
2026-09-23 20:16:59 +0200

    Removed unused imports

--- src/main/java/edivad/dimstorage/datagen/loot/DimStorageLootTableProvider.java
+++ src/main/java/edivad/dimstorage/datagen/loot/DimStorageLootTableProvider.java
@@ -2,10 +2,7 @@ package edivad.dimstorage.datagen.loot;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.SingleRegistryBootstrap;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
